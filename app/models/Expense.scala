8c9c505268f255bd232445125db63ea84908644f
bruh7463 <155955922+bruh7463@users.noreply.github.com>
2024-06-11 17:34:06 +0200

    Update Expense.scala

--- app/models/Expense.scala
+++ app/models/Expense.scala
@@ -1,3 +1,4 @@
 package models
 
+//This is the model that play framework uses when creating and sending an expense to the database
 case class Expense(username: String, item: String, cost: Double, quantity: Int, location: String)
