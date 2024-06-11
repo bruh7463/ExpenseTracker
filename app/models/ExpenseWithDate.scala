f43e75cf650e81c293f6a97c491ba5d3642ebc33
bruh7463 <155955922+bruh7463@users.noreply.github.com>
2024-06-11 17:36:47 +0200

    Update ExpenseWithDate.scala

--- app/models/ExpenseWithDate.scala
+++ app/models/ExpenseWithDate.scala
@@ -2,8 +2,10 @@ package models
 
 import play.api.libs.json.{Json, OFormat}
 
+// This is the model that play framework uses when retrieving expenses from the database
 case class ExpenseWithDate(username: String, item: String, cost: Double, quantity: Int, location: String, date: String)
 
+// This object helps to convert the expenses data retrieved from the database into json format for them to be displayed as a graph
 object ExpenseWithDate {
   implicit val format: OFormat[ExpenseWithDate] = Json.format[ExpenseWithDate]
 }
