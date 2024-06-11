e056076b084d08fb5b6125bcb453a07f31b5fd6a
bruh7463 <155955922+bruh7463@users.noreply.github.com>
2024-06-11 18:00:44 +0200

    Update Dashboard.scala

--- app/controllers/Dashboard.scala
+++ app/controllers/Dashboard.scala
@@ -1,3 +1,5 @@
+//This is where everything to do with the dashboard page is done
+
 package controllers
 
 import javax.inject._
@@ -11,6 +13,7 @@ import play.api.libs.json.Json
 @Singleton
 class Dashboard @Inject()(cc: MessagesControllerComponents, calExpense: Expenses)(implicit ec: ExecutionContext) extends MessagesAbstractController(cc) {
 
+  //This method displays the dashboard page with the users' expense data from the database
   def dashboard: Action[AnyContent] = Action.async { implicit request: MessagesRequest[AnyContent] =>
     request.session.get("username") match {
       case Some(username) =>
