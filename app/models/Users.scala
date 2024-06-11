93d9b6150c32f9ddabe4ed3c9269b86eb5787d9b
bruh7463 <155955922+bruh7463@users.noreply.github.com>
2024-06-11 17:45:17 +0200

    Update Users.scala

--- app/models/Users.scala
+++ app/models/Users.scala
@@ -1,4 +1,4 @@
 package models
 
-// This model helps in retrieving the users info from the database
+// This model helps in retrieving, creating and sending the users info to and from the database
 case class Users(username: String, password: String, securityQuestion: String, securityAnswer: String, income: Double, incomeSource: String)
