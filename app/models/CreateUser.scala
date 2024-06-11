6606290073353f2fcbbda4778fb96aaf5510ba39
bruh7463 <155955922+bruh7463@users.noreply.github.com>
2024-06-11 17:43:49 +0200

    Update CreateUser.scala

--- app/models/CreateUser.scala
+++ app/models/CreateUser.scala
@@ -1,4 +1,5 @@
+//This is a dummy model used for testing purposes
+
 package models
 
-// This is the model that play framework uses when creating and sending a user's details to the database
 case class CreateUser(username: String, password: String, securityQuestion: String, securityAnswer: String, income: Double, incomeSource: String)
